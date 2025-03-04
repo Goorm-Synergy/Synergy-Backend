11991be825f9010f8665784fda2d07c723eb8905
leeminjung1 <vipminjung@gmail.com>
2025-03-04 14:22:28 +0900

    refactor: switch to @RestControllerAdvice for API response formatting

--- src/main/java/com/synergy/backend/global/common/ApiResponseAdvice.java
+++ src/main/java/com/synergy/backend/global/common/ApiResponseAdvice.java
@@ -6,10 +6,10 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiResponseAdvice implements ResponseBodyAdvice<ApiResponse<?>> {
 
 	@Override
