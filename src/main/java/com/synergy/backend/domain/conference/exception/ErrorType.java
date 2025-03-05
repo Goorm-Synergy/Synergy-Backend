41c83d5082564a620f4ed5872067b14d55ce8aca
wjd4204 <brian876373@gmail.com>
2025-03-05 15:55:38 +0900

    feat: Conference Exception 추가

--- src/main/java/com/synergy/backend/domain/conference/exception/ErrorType.java
+++ src/main/java/com/synergy/backend/domain/conference/exception/ErrorType.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 public enum ErrorType {
 
     INVALID_TIME_PERIOD(400, "시작 날짜는 더 빨라야 합니다."),
+    _NOT_FOUND_CONFERENCE(400, "해당 컨퍼런스를 찾을 수 없습니다.")
     ;
 
     private final int code;
