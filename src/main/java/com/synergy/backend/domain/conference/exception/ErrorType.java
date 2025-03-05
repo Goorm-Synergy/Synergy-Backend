ebacbae9f68ec3298921a90c08ddc224be729ea5
Kimseungin0529 <whffkaos007@naver.com>
2025-03-05 16:00:55 +0900

    style : ErrorTpye 변수명 규칙 통합으로 인한 수정

--- src/main/java/com/synergy/backend/domain/conference/exception/ErrorType.java
+++ src/main/java/com/synergy/backend/domain/conference/exception/ErrorType.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorType {
 
-    INVALID_TIME_PERIOD(400, "시작 날짜는 더 빨라야 합니다."),
+    _INVALID_TIME_PERIOD(400, "시작 날짜는 더 빨라야 합니다."),
     _NOT_FOUND_CONFERENCE(400, "해당 컨퍼런스를 찾을 수 없습니다.")
     ;
 
