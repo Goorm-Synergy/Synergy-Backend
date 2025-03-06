55f36e4eacd1857c54faeb6c2c7580af962d20f6
Kimseungin0529 <whffkaos007@naver.com>
2025-03-06 15:18:18 +0900

    refactor : ConferenceServiceImpl update 메서드 추상화 작업

--- src/main/java/com/synergy/backend/domain/conference/service/ConferenceServiceImpl.java
+++ src/main/java/com/synergy/backend/domain/conference/service/ConferenceServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.time.Period;
 import java.util.Optional;
 
 @Service
@@ -22,7 +21,6 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class ConferenceServiceImpl implements ConferenceService {
     private final ConferenceRepository conferenceRepository;
-    // private final SessionRepository sessionRepository
 
     @Transactional
     @Override
@@ -37,19 +35,22 @@ public class ConferenceServiceImpl implements ConferenceService {
     @Transactional
     @Override
     public ConferenceUpdateResponse update(Long conferenceId, ConferenceUpdateRequest request) {
-        // 컨퍼런스 조회 -> 예외 처리
         Conference findConference = conferenceRepository.findById(conferenceId)
                 .orElseThrow(NotFoundConference::new);
-        //dto 필드 유무 확인
-        Optional.ofNullable(request.getName()).ifPresent((name) -> findConference.updateName(name));
-        Optional<LocalDateTime> startTime = Optional.ofNullable(request.getStartTime());
-        Optional<LocalDateTime> endTime = Optional.ofNullable(request.getEndTime());
-        if(startTime.isPresent() && endTime.isPresent()) {
-            findConference.updatePeriod(TimePeriod.of(startTime.get(), endTime.get()));
-        }
-        Optional.ofNullable(request.getLocation()).ifPresent((location) -> findConference.updateLocation(location));
-        // 컨퍼런스 정보 변경
+        applyUpdatesToConference(request, findConference);
 
         return ConferenceUpdateResponse.from(findConference); // 반영된 정보 반환
+    }
+
+    private void applyUpdatesToConference(ConferenceUpdateRequest request, Conference findConference) {
+        Optional.ofNullable(request.getName()).ifPresent(findConference::updateName);
+        Optional.ofNullable(request.getLocation()).ifPresent(findConference::updateLocation);
+
+        Optional<LocalDateTime> optionalStartTime = Optional.ofNullable(request.getStartTime());
+        Optional<LocalDateTime> optionalEndTime = Optional.ofNullable(request.getEndTime());
+        if(optionalStartTime.isPresent() && optionalEndTime.isPresent()) {
+            findConference.updatePeriod(TimePeriod.of(optionalStartTime.get(), optionalEndTime.get()));
+        }
+
     }
 }
