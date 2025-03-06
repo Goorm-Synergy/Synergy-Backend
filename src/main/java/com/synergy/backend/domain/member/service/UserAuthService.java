99cf8e6108c210f47d89a0afd05be3615bd383f9
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2025-03-06 09:34:39 +0900

    refactor: Restructure Member table schema (#9)
    * refactor: Restructure Member table schema
    
    * refactor: Introduce MemberRepository as a base repository
    
    * refactor: Rename adminId to assignedAdminId
    
    * feat: add phone column

---
+++ src/main/java/com/synergy/backend/domain/member/service/UserAuthService.java
@@ -0,0 +1,13 @@
+package com.synergy.backend.domain.member.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserAuthService {
+
+}
