10e0e0052e082c945013804171f612b87b90a979
Kimseungin0529 <whffkaos007@naver.com>
2025-03-10 16:06:32 +0900

    refator/test : Conference 기존 테스트 변경 / 추가된 값에 대한 테스트 구현

--- src/test/java/com/synergy/backend/domain/conference/entity/ConferenceTest.java
+++ src/test/java/com/synergy/backend/domain/conference/entity/ConferenceTest.java
@@ -40,25 +40,57 @@ class ConferenceTest {
     Collection<DynamicTest> ofExceptionName() {
         // given
         TimePeriod timePeriod = TimePeriod.of(LocalDateTime.of(2025, 4, 14, 9, 0), LocalDateTime.of(2025, 4, 15, 16, 0));
+        String location = "컨퍼런스 위치";
+        String organizer = "김승진";
+        String type = "산업";
 
         // when & then
         return List.of(
                 DynamicTest.dynamicTest("컨퍼런스 제목은 공백으로 작성할 수 없습니다..", () -> {
                             //given
                             String name = "  ";
-                            String location = "컨퍼런스 위치";
                             // when & then
-                            assertThatThrownBy(() -> Conference.of(name, timePeriod, location))
+                            assertThatThrownBy(() -> Conference.of(name, timePeriod, organizer, location, type))
                                     .hasMessage(_INVALID_CONFERENCE_NAME.getMessage())
                                     .isInstanceOf(InvalidNameException.class);
                         }
                 ),
-                DynamicTest.dynamicTest("컨퍼런스 제목은 50자 이내 입니다.", () -> {
+                DynamicTest.dynamicTest("컨퍼런스 제목은 30자 이내 입니다.", () -> {
                             //given
-                            String name = "123456789/123456789/123456789/123456789/123456789/1";
-                            String location = "컨퍼런스 위치";
+                            String name = "123456789/123456789/123456789/1";
                             // when & then
-                            assertThatThrownBy(() -> Conference.of(name, timePeriod, location))
+                            assertThatThrownBy(() -> Conference.of(name, timePeriod, organizer, location, type))
+                                    .hasMessage(_INVALID_CONFERENCE_NAME.getMessage())
+                                    .isInstanceOf(InvalidNameException.class);
+                        }
+                )
+        );
+    }
+
+    @DisplayName("잘못된 주최자명 형식으로 등록하는 시나리오")
+    @TestFactory
+    Collection<DynamicTest> ofExceptionOrganizer() {
+        // given
+        TimePeriod timePeriod = TimePeriod.of(LocalDateTime.of(2025, 4, 14, 9, 0), LocalDateTime.of(2025, 4, 15, 16, 0));
+        String name = "카카오 IT 컨퍼런스";
+        String location = "컨퍼런스 위치";
+        String type = "산업";
+        // when & then
+        return List.of(
+                DynamicTest.dynamicTest("주최자명은 공백으로 작성할 수 없습니다..", () -> {
+                            //given
+                            String organizer = " ";
+                            // when & then
+                            assertThatThrownBy(() -> Conference.of(name, timePeriod, organizer, location, type))
+                                    .hasMessage(_INVALID_CONFERENCE_NAME.getMessage())
+                                    .isInstanceOf(InvalidNameException.class);
+                        }
+                ),
+                DynamicTest.dynamicTest("컨퍼런스 제목은 30자 이내 입니다.", () -> {
+                            //given
+                            String organizer = "김 승진 수수깡, 김 승진 수수깡, 김 승진 수수깡, 김 승진 수수깡, 김 승진 수수깡, ";
+                            // when & then
+                            assertThatThrownBy(() -> Conference.of(name, timePeriod, organizer, location, type))
                                     .hasMessage(_INVALID_CONFERENCE_NAME.getMessage())
                                     .isInstanceOf(InvalidNameException.class);
                         }
@@ -71,27 +103,26 @@ class ConferenceTest {
     Collection<DynamicTest> ofExceptionLocation() {
         // given
         TimePeriod timePeriod = TimePeriod.of(LocalDateTime.of(2025, 4, 14, 9, 0), LocalDateTime.of(2025, 4, 15, 16, 0));
-
+        String name = "카카오 IT 컨퍼런스";
+        String organizer = "김승진";
+        String type = "산업";
         // when & then
         return List.of(
                 DynamicTest.dynamicTest("컨퍼런스 위치는 공백으로 작성할 수 없습니다..", () -> {
                             //given
-                            String name = "컨퍼런스명";
                             String location = "  ";
                             // when & then
-                            assertThatThrownBy(() -> Conference.of(name, timePeriod, location))
+                            assertThatThrownBy(() -> Conference.of(name, timePeriod, organizer, location, type))
                                     .hasMessage(_INVALID_CONFERENCE_LOCATION.getMessage())
                                     .isInstanceOf(InvalidLocationException.class);
                         }
                 ),
                 DynamicTest.dynamicTest("컨퍼런스 위치는 100자 이내 입니다.", () -> {
                             //given
-                            String name = "컨퍼런스명";
-                            String location = "123456789/123456789/123456789/123456789/123456789" +
-                                    "/123456789/123456789/123456789/123456789/123456789/1";
+                            String location = "지구에 존재하는 동양 국가로 대한민국 서울특별시에 존재하는 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구저쩌구저쩌구저쩌구저쩌구저쩌구저쩌구저쩌구  마포에 있는 커스텀 건물";
 
                             // when & then
-                            assertThatThrownBy(() -> Conference.of(name, timePeriod, location))
+                            assertThatThrownBy(() -> Conference.of(name, timePeriod, organizer, location, type))
                                     .hasMessage(_INVALID_CONFERENCE_LOCATION.getMessage())
                                     .isInstanceOf(InvalidLocationException.class);
                         }
@@ -99,4 +130,34 @@ class ConferenceTest {
         );
     }
 
+    @DisplayName("잘못된 유형 형식으로 등록하는 시나리오")
+    @TestFactory
+    Collection<DynamicTest> ofExceptionTpye() {
+        // given
+        TimePeriod timePeriod = TimePeriod.of(LocalDateTime.of(2025, 4, 14, 9, 0), LocalDateTime.of(2025, 4, 15, 16, 0));
+        String name = "카카오 IT 컨퍼런스";
+        String organizer = "김승진";
+        String location = "서울 마포대로 지하 축제 공간";
+        // when & then
+        return List.of(
+                DynamicTest.dynamicTest("유형은 공백으로 작성할 수 없습니다..", () -> {
+                            //given
+                            String type = "  ";
+                            // when & then
+                            assertThatThrownBy(() -> Conference.of(name, timePeriod, organizer, location, type))
+                                    .hasMessage(_INVALID_CONFERENCE_NAME.getMessage())
+                                    .isInstanceOf(InvalidNameException.class);
+                        }
+                ),
+                DynamicTest.dynamicTest("유형은 50자 이내 입니다.", () -> {
+                            //given
+                            String type = "  ";
+                            // when & then
+                            assertThatThrownBy(() -> Conference.of(name, timePeriod, organizer, location, type))
+                                    .hasMessage(_INVALID_CONFERENCE_NAME.getMessage())
+                                    .isInstanceOf(InvalidNameException.class);
+                        }
+                )
+        );
+    }
 }
