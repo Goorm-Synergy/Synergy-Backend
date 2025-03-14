2ba8b538e69b4e0b4dcfb669391544568b93bbc4
leeminjung1 <vipminjung@gmail.com>
2025-03-14 15:32:45 +0900

    test: PointServiceImpleTest add testAddPoint

--- src/test/java/com/synergy/backend/domain/point/service/PointServiceImplTest.java
+++ src/test/java/com/synergy/backend/domain/point/service/PointServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,20 +44,20 @@ class PointServiceImplTest {
 	@Mock
 	private RecruiterRepository recruiterRepository;
 
-	private Attendee attendee;
-	private Point point;
+	private Attendee testAttendee;
+	private Point testPoint;
 
 	@BeforeEach
 	void setUp() {
-		attendee = Attendee.of("email", "pass", "name", "0101");
-		point = Point.of(PointType.SIGN_UP);
+		testAttendee = Attendee.of("email", "pass", "name", "0101");
+		testPoint = Point.of(PointType.SIGN_UP);
 	}
 
 	@DisplayName("사용자의 포인트 내역을 조회할 수 있다.")
 	@Test
 	void testGetPointHistory() {
 		// given
-		List<Point> points = List.of(point);
+		List<Point> points = List.of(testPoint);
 		when(pointRepository.findByAttendeeIdOrderByCreatedTimeDesc(1L)).thenReturn(points);
 
 		// when
@@ -64,7 +65,71 @@ class PointServiceImplTest {
 
 		// then
 		assertEquals(1, result.size());
-		assertEquals(point, result.get(0));
+		assertEquals(testPoint, result.get(0));
+	}
+
+	@DisplayName("부스 참여 타입의 포인트가 추가된다.")
+	@Test
+	void testAddBoothAttendPoint() {
+		// given
+		when(attendeeRepository.findById(1L)).thenReturn(Optional.of(testAttendee));
+
+		// when
+		pointService.addBoothPoint(1L, 200L);
+
+		// then
+		verify(pointRepository, times(1)).save(any(Point.class));
+		verify(attendeeRepository, times(1)).save(testAttendee);
+		assertEquals(1, testAttendee.getPoints().size());
+		assertEquals(PointType.BOOTH_VISIT, testAttendee.getPoints().get(0).getPointType());
+	}
+
+	@DisplayName("세션 참여 타입의 포인트가 추가된다.")
+	@Test
+	void testAddSessionAttendPoint() {
+		// given
+		when(attendeeRepository.findById(1L)).thenReturn(Optional.of(testAttendee));
+
+		// when
+		pointService.addSessionAttendPoint(1L, 200L);
+
+		// then
+		verify(pointRepository, times(1)).save(any(Point.class));
+		verify(attendeeRepository, times(1)).save(testAttendee);
+		assertEquals(1, testAttendee.getPoints().size());
+		assertEquals(PointType.SESSION_ATTEND, testAttendee.getPoints().get(0).getPointType());
+	}
+
+	@DisplayName("세션 QnA 타입의 포인트가 추가된다.")
+	@Test
+	void testAddSessionQnAPoint() {
+		// given
+		when(attendeeRepository.findById(1L)).thenReturn(Optional.of(testAttendee));
+
+		// when
+		pointService.addSessionQnaPoint(1L, 200L);
+
+		// then
+		verify(pointRepository, times(1)).save(any(Point.class));
+		verify(attendeeRepository, times(1)).save(testAttendee);
+		assertEquals(1, testAttendee.getPoints().size());
+		assertEquals(PointType.SESSION_QNA, testAttendee.getPoints().get(0).getPointType());
+	}
+
+	@DisplayName("회원가입 타입의 포인트가 추가된다.")
+	@Test
+	void testAddSignupPoint() {
+		// given
+		when(attendeeRepository.findById(1L)).thenReturn(Optional.of(testAttendee));
+
+		// when
+		pointService.addSignupPoint(1L);
+
+		// then
+		verify(pointRepository, times(1)).save(any(Point.class));
+		verify(attendeeRepository, times(1)).save(testAttendee);
+		assertEquals(1, testAttendee.getPoints().size());
+		assertEquals(PointType.SIGN_UP, testAttendee.getPoints().get(0).getPointType());
 	}
 
 }
