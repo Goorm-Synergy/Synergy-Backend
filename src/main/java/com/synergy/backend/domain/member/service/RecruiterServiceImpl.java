eaa34ad5910be8e17966ea7032e047fe251f36be
Kimseungin0529 <whffkaos007@naver.com>
2025-03-17 10:53:26 +0900

    RecruiterServiceImpl findAttendeeFrom 메소드 구현

--- src/main/java/com/synergy/backend/domain/member/service/RecruiterServiceImpl.java
+++ src/main/java/com/synergy/backend/domain/member/service/RecruiterServiceImpl.java
@@ -2,6 +2,10 @@ package com.synergy.backend.domain.member.service;
 
 import com.synergy.backend.domain.member.api.dto.AttendeeDetailResponseDto;
 import com.synergy.backend.domain.member.api.dto.AttendeeListResponseDto;
+import com.synergy.backend.domain.member.entity.Attendee;
+import com.synergy.backend.domain.member.exception.ForbiddenHiringUserException;
+import com.synergy.backend.domain.member.exception.NotFoundUserException;
+import com.synergy.backend.domain.member.repository.AttendeeRepository;
 import com.synergy.backend.domain.member.repository.RecruiterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,10 +17,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecruiterServiceImpl implements RecruiterService {
 
     private final RecruiterRepository recruiterRepository;
+    private final AttendeeRepository attendeeRepository;
+
 
     @Override
     public AttendeeDetailResponseDto findAttendeeFrom(String identifier, Long id) {
-        return null;
+        Attendee attendee = attendeeRepository.findById(id).orElseThrow(NotFoundUserException::new);
+        if (!attendee.isHiringInterested()) {
+            throw new ForbiddenHiringUserException();
+        }
+
+        return AttendeeDetailResponseDto.of(attendee);
     }
 
     @Override
