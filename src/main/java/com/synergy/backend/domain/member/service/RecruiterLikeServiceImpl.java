730a112d992647359f1b0706690e4528fe1b04dc
leeminjung1 <vipminjung@gmail.com>
2025-03-19 20:46:06 +0900

    feat: add controller and service

---
+++ src/main/java/com/synergy/backend/domain/member/service/RecruiterLikeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.synergy.backend.domain.member.service;
+
+public class RecruiterLikeServiceImpl implements RecruiterLikeService {
+
+	@Override
+	public void likeAttendee(Long id, Long attendeeId) {
+
+	}
+
+	@Override
+	public void unlikeAttendee(Long id, Long attendeeId) {
+
+	}
+}
