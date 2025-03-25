faa8a0e0ad5f274e8bcb1623f65b2b923211a36d
Kimseungin0529 <whffkaos007@naver.com>
2025-03-25 18:16:29 +0900

    fix : 채용담당자 사용자 목록 조회 수정

--- src/main/java/com/synergy/backend/domain/member/repository/AttendeeRepositoryImpl.java
+++ src/main/java/com/synergy/backend/domain/member/repository/AttendeeRepositoryImpl.java
@@ -56,7 +56,7 @@ public class AttendeeRepositoryImpl implements AttendeeRepositoryCustom {
 				.and(recruiterAttendeeLike.recruiter.id.eq(recruiterId))) // 현재 로그인 리크루터의 좋아요 여부
 			.join(attendee.desiredJobPosition, jobPosition)
 			.where(
-				occupationIn(requestCondition.desiredOccupations()),
+				jobPositionIn(requestCondition.desiredJobPosition()),
 				educationEq(requestCondition.educationLevel()),
 				ageGroupEq(requestCondition.ageGroup()),
 				experienceEq(requestCondition.experienceLevel()),
@@ -70,8 +70,9 @@ public class AttendeeRepositoryImpl implements AttendeeRepositoryCustom {
 		return new PageImpl<>(content, pageable, content.size());
 	}
 
-	private BooleanExpression occupationIn(List<String> occupations) {
-		return (occupations == null || occupations.isEmpty()) ? null : attendee.currentJobPosition.name.in(occupations);
+	private BooleanExpression jobPositionIn(List<String> jobPositions) {
+		return (jobPositions == null || jobPositions.isEmpty()) ? null :
+			attendee.desiredJobPosition.name.in(jobPositions);
 
 	}
 
