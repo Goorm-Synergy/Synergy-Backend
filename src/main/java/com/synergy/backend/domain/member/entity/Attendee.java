a45065be2f72e2a16cdc82837d1cda15caced48c
leeminjung1 <vipminjung@gmail.com>
2025-03-26 01:09:30 +0900

    feat: add isLocked field

--- src/main/java/com/synergy/backend/domain/member/entity/Attendee.java
+++ src/main/java/com/synergy/backend/domain/member/entity/Attendee.java
@@ -69,6 +69,10 @@ public class Attendee extends BaseEntity implements User {
 	@Column(nullable = false)
 	private String phone;
 
+	// 계정 잠금 상태
+	@Column(nullable = false)
+	private boolean isLocked;
+
 	// 현재 포인트 합계
 	@Column(nullable = false)
 	private int totalPoints = 0;
