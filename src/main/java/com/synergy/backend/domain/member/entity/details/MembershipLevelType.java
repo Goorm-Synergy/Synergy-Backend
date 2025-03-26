67b56ddaf2405927489b40447463db18798912ea
leeminjung1 <vipminjung@gmail.com>
2025-03-26 10:02:42 +0900

    refactor: add static int membership point

--- src/main/java/com/synergy/backend/domain/member/entity/details/MembershipLevelType.java
+++ src/main/java/com/synergy/backend/domain/member/entity/details/MembershipLevelType.java
@@ -9,25 +9,29 @@ public enum MembershipLevelType {
 	BRONZE,
 	DEFAULT;
 
+	private static final int BRONZE_THRESHOLD = 100;
+	private static final int SILVER_THRESHOLD = 300;
+	private static final int GOLD_THRESHOLD = 800;
+
 	public static MembershipLevelType getMembershipLevel(int point) {
-		if (point >= 800)
+		if (point >= GOLD_THRESHOLD)
 			return GOLD;
-		if (point >= 300)
+		if (point >= SILVER_THRESHOLD)
 			return SILVER;
-		if (point >= 100)
+		if (point >= BRONZE_THRESHOLD)
 			return BRONZE;
 		return DEFAULT;
 	}
 
 	public static NextPointInfo getNextLevelInfo(int point) {
-		if (point >= 800)
+		if (point >= GOLD_THRESHOLD)
 			return new NextPointInfo(PLATINUM, 0);
-		if (point >= 300)
-			return new NextPointInfo(GOLD, 800 - point);
-		if (point >= 100)
-			return new NextPointInfo(SILVER, 300 - point);
+		if (point >= SILVER_THRESHOLD)
+			return new NextPointInfo(GOLD, GOLD_THRESHOLD - point);
+		if (point >= BRONZE_THRESHOLD)
+			return new NextPointInfo(SILVER, SILVER_THRESHOLD - point);
 		if (point >= 0)
-			return new NextPointInfo(BRONZE, 100 - point);
+			return new NextPointInfo(BRONZE, BRONZE_THRESHOLD - point);
 		return new NextPointInfo(DEFAULT, 0);
 	}
 }
