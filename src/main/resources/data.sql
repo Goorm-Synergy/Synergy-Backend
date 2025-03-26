6e283663970f4e245046f4530662c4b24b239c4e
leeminjung1 <vipminjung@gmail.com>
2025-03-26 21:03:54 +0900

    fix: rearrange data.sql Insert conferece first

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,6 +14,7 @@ VALUES
     (250, '2025-09-15', 1, '2025-09-15 15:00', '2025-09-15 14:00', '클라우드 솔루션 전문가', '박찬영', '클라우드 네이티브, 아키텍처의 미래', '빠르게 변화하는 IT 산업에서 최신 기술 동향을 파악하는 것은 기업의 경쟁력을 높이고 미래를 준비하는 데 필수적입니다. 기업의 CTO가 AI, 클라우드, Web3 등 주요 기술 트렌드와 산업 변화를 분석하고, 기업이 기술 혁신을 어떻게 주도할 수 있는지에 대한 전략과 인사이트를 제공합니다.', ' ', ' ', ' ', 'https://synergy-conference-bucket.s3.ap-northeast-2.amazonaws.com/all/dummydata+v.0.1/session-random/unsplash_VjzXKgCUqLw.png', ' '),
     (250, '2025-09-15', 1, '2025-09-15 16:00', '2025-09-15 16:00', 'NextWork 프로덕트 매니저', '이경욱', '원격 근무 환경에서의 생산성 향상 전략', '빠르게 변화하는 IT 산업에서 최신 기술 동향을 파악하는 것은 기업의 경쟁력을 높이고 미래를 준비하는 데 필수적입니다. 기업의 CTO가 AI, 클라우드, Web3 등 주요 기술 트렌드와 산업 변화를 분석하고, 기업이 기술 혁신을 어떻게 주도할 수 있는지에 대한 전략과 인사이트를 제공합니다.', ' ', ' ', ' ', 'https://synergy-conference-bucket.s3.ap-northeast-2.amazonaws.com/all/dummydata+v.0.1/session-random/unsplash_wn7dOzUh3Rs.png', ' ');
 
+
 -- id값 테이블은 1부터, enum은 0부터
 ---- 관심 분야
 INSERT INTO interest (name, code) VALUES ('데이터 분석 / AI', 101);
@@ -99,6 +100,7 @@ INSERT INTO attendee_desired_work_region (attendee_id, desired_work_region) VALU
 -- 김다혜 (attendee_id = 6) → 울산, 충청
 INSERT INTO attendee_desired_work_region (attendee_id, desired_work_region) VALUES (6, 5); -- ULSAN
 INSERT INTO attendee_desired_work_region (attendee_id, desired_work_region) VALUES (6, 8); -- CHUNGCHEONG
+
 
 
 -- 참가자의 현재 직업 및 직무 업데이트
