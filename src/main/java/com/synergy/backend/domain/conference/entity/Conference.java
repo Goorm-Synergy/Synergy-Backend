f74206789ed803b3ddea6a79997a96b62ea2877c
leeminjung1 <vipminjung@gmail.com>
2025-03-26 17:21:51 +0900

    feat: add ticketCode field into Conference

--- src/main/java/com/synergy/backend/domain/conference/entity/Conference.java
+++ src/main/java/com/synergy/backend/domain/conference/entity/Conference.java
@@ -50,6 +50,9 @@ public class Conference {
     @Column(nullable = false, length = MAX_COMMON_LENGTH)
     private String type;
 
+    @Column
+    private String ticketCode;
+
     @OneToMany(mappedBy = "conference", cascade = {CascadeType.PERSIST, REMOVE}, orphanRemoval = true)
     private List<Session> sessions = new ArrayList<>();
 
