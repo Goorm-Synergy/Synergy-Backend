74a6032dd2bbf3ce6d87d3a85307bed26dc90b2a
leeminjung1 <vipminjung@gmail.com>
2025-03-27 14:58:58 +0900

    chore: Remove extractRefreshToken method since CookieValue is used instead

--- src/main/java/com/synergy/backend/global/token/CookieUtils.java
+++ src/main/java/com/synergy/backend/global/token/CookieUtils.java
@@ -15,17 +15,6 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CookieUtils {
 	public static final String REFRESH_TOKEN_NAME = "refreshToken";
 
-	public String extractRefreshToken(HttpServletRequest request) {
-		if (request.getCookies() == null)
-			return null;
-
-		return Arrays.stream(request.getCookies())
-			.filter(cookie -> REFRESH_TOKEN_NAME.equals(cookie.getName()))
-			.map(Cookie::getValue)
-			.findFirst()
-			.orElse(null);
-	}
-
 	public void addRefreshTokenToCookie(HttpServletResponse response, String refreshToken) {
 		ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN_NAME, refreshToken)
 			.httpOnly(true)
