166432e63d0e0db84218a430ec8c41a479a958c3
leeminjung1 <vipminjung@gmail.com>
2025-03-31 10:46:00 +0900

    chore: change data.sql

--- src/main/java/com/synergy/backend/SynergyBackendApplication.java
+++ src/main/java/com/synergy/backend/SynergyBackendApplication.java
@@ -1,14 +1,7 @@
 package com.synergy.backend;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.synergy.backend.global.util.JwtBatchGenerator;
-
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SynergyBackendApplication {
@@ -16,10 +9,4 @@ public class SynergyBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SynergyBackendApplication.class, args);
 	}
-	// @Autowired
-	// private JwtBatchGenerator jwtBatchGenerator;
-	// @PostConstruct
-	// public void init() throws IOException {
-	// 	jwtBatchGenerator.generateTokensToCsv();
-	// }
 }
